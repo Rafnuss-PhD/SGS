7d6bd7c487b05f5e336877e53303c452973f9744
Raphael Nussbaumer (ISTE) <rafnuss@gmail.com>
2017-08-17 11:47:18 +0200

    add varcovar

---
+++ SGS_varcovar.m
@@ -0,0 +1,165 @@
+function CY = SGS_varcovar(nx,ny,parm)
+
+
+%% * *INPUT CEHCKING*
+% This section of the code generates a valid parm structure based on the
+% inputted parm. If some parm value haven't been input, this section will
+% fill automatically with defautl value. This may not allowed be the best.
+
+
+% Paramter settings
+if ~isfield(parm, 'seed_path'),     parm.seed_path      = 'shuffle'; end
+if ~isfield(parm, 'seed_U'),        parm.seed_U         = 'shuffle'; end
+if ~isfield(parm, 'saveit'),        parm.saveit         = 1; end % bolean, save or not the result of simulation
+if ~isfield(parm, 'name'),          parm.name           = ''; end % name use for saving file
+if ~isfield(parm, 'n_real'),        parm.n_real  = 1; end
+
+% Kriging parameter
+parm.k.covar = kriginginitiaite(parm.k.covar);
+if ~isfield(parm, 'k') || ~isfield(parm.k, 'method'),  parm.k.method = 'sbss'; end
+if ~isfield(parm, 'k') || ~isfield(parm.k, 'nb'),  parm.k.nb = 30; end
+
+if ~isfield(parm, 'k') || ~isfield(parm.k, 'wradius')
+    parm.k.wradius  = 3;
+end
+k = parm.k;
+
+
+% Path
+if ~isfield(parm, 'path'),          parm.path            = 'linear'; end
+if ~isfield(parm, 'path_random'),   parm.path_random     = 1; end
+if ~isfield(parm, 'mg'),            parm.mg              = 1; end
+
+
+if ~isfield(parm, 'cstk_s') % cstk_s is the scale at which cst is switch on
+    if ~isfield(parm, 'cstk'),      parm.cstk           = 1; end % constant path and kriging weight activated or not
+    if parm.cstk
+        parm.cstk_s = 0; % will never use cstk
+    else
+        parm.cstk_s = Inf; % will always use cstk
+    end
+end
+
+
+
+%% 1. Creation of the grid an path
+[Y, X] = ndgrid(1:ny,1:nx,'uint32');
+
+%% 2. Define Path
+Path = nan(ny,nx, 'uint32');
+rng(parm.seed_path);
+if parm.mg
+   sx = 1:ceil(log(nx+1)/log(2));
+   sy = 1:ceil(log(ny+1)/log(2));
+   sn = max([numel(sy), numel(sx)]);
+   nb = nan(sn,1);
+   start = zeros(sn+1,1);
+   dx = nan(sn,1); dy = nan(sn,1);
+   path = nan(nx*ny,1, 'uint32');
+   for i_scale = 1:sn
+       dx(i_scale) = 2^(sn-sx(min(i_scale,end)));
+       dy(i_scale) = 2^(sn-sy(min(i_scale,end)));
+       [Y_s,X_s] = ndgrid(1:dy(i_scale):ny,1:dx(i_scale):nx); % matrix coordinate
+       id = find(isnan(Path(:)) & ismember([Y(:) X(:)], [Y_s(:) X_s(:)], 'rows'));
+       nb(i_scale) = numel(id);
+       start(i_scale+1) = start(i_scale)+nb(i_scale);
+       path( start(i_scale)+(1:nb(i_scale)) ) = id(randperm(nb(i_scale)));
+       Path(path( start(i_scale)+(1:nb(i_scale)) )) = start(i_scale)+(1:nb(i_scale));
+   end
+else
+   id=find(isnan(Path));
+   path = id(randperm(numel(id)));
+   Path(path) = 1:numel(id);
+   dx=1; dy=1; nb = numel(id); start=[0 nb]; sn=1;
+end
+
+
+%% 3. Initialization Spiral Search
+
+% Initialize spiral search stuff which don't change
+x = ceil( min(k.covar(1).range(1)*k.wradius, nx));
+y = ceil( min(k.covar(1).range(2)*k.wradius, ny));
+[ss_Y, ss_X] = ndgrid(-y:y, -x:x);% grid{i_scale} of searching windows
+ss_dist = sqrt( (ss_X/k.covar(1).range(1)).^2 + (ss_Y/k.covar(1).range(2)).^2); % find distence
+ss_id_1 = find(ss_dist <= k.wradius); % filter node behind radius.
+rng(parm.seed_search);
+ss_id_1 = ss_id_1(randperm(numel(ss_id_1)));
+[~, ss_id_2] = sort(ss_dist(ss_id_1)); % sort according distence.
+ss_X_s=ss_X(ss_id_1(ss_id_2)); % sort the axis
+ss_Y_s=ss_Y(ss_id_1(ss_id_2));
+ss_n=numel(ss_X_s); %number of possible neigh
+
+
+if parm.mg
+    ss_scale=sn*ones(size(ss_X));
+    for i_scale = sn-1:-1:1
+        x_s = [-fliplr(dx(i_scale):dx(i_scale):x(end)) 0 dx(i_scale):dx(i_scale):x(end)]+(x+1);
+        y_s = [-fliplr(dy(i_scale):dy(i_scale):y(end)) 0 dy(i_scale):dy(i_scale):y(end)]+(y+1);
+        ss_scale(y_s,x_s)=i_scale;
+    end
+    ss_scale_s = ss_scale(ss_id_1(ss_id_2));
+else
+    ss_scale_s = sn*ones(size(ss_id_2));
+end
+
+%% 3. Initialization Covariance Lookup Table
+ss_a0_C = zeros(ss_n,1);
+ss_ab_C = zeros(ss_n);
+for i=1:numel(k.covar)
+    a0_h = sqrt(sum(([ss_Y_s(:) ss_X_s(:)]*k.covar(i).cx).^2,2));
+    ab_h = squareform(pdist([ss_Y_s ss_X_s]*k.covar(i).cx));
+    
+    ss_a0_C = ss_a0_C + kron(k.covar(i).g(a0_h), k.covar(i).c0);
+    ss_ab_C = ss_ab_C + kron(k.covar(i).g(ab_h), k.covar(i).c0);
+end
+% Transform ss.ab_C sparse?
+
+%% 3. Initialization Covariance Lookup Table
+
+                     
+LambdaM = zeros(nx*ny,k.nb+1);
+
+k_covar_c0 = sum([k.covar.c0]);
+XY_i=[Y(path) X(path)];
+
+%% 3. Loop
+
+for i_scale = 1:sn
+    ss_id = find(ss_scale_s<=i_scale);
+    ss_XY_s = [ss_Y_s(ss_id) ss_X_s(ss_id)];
+    ss_a0_C_s = ss_a0_C(ss_id);
+    ss_ab_C_s = ss_ab_C(ss_id,ss_id);
+    
+    for i_pt = start(i_scale)+(1:nb(i_scale))
+        n=0;
+        neigh=nan(k.nb,3);
+        for nn = 2:size(ss_XY_s,1) % 1 is the point itself... therefore unknown
+            ijt = XY_i(i_pt,:)+ss_XY_s(nn,:);
+            if ijt(1)>0 && ijt(2)>0 && ijt(1)<=ny && ijt(2)<=nx 
+                if Path(ijt(1),ijt(2)) < i_pt % check if it,jt exist
+                    n=n+1;
+                    neigh(n,:) = [nn ijt];
+                    if n >= k.nb
+                        break;
+                    end
+                end
+            end
+        end
+        
+        neigh_id = (neigh(1:n,2:3)-1)*[1 ny]'+1;
+        
+        a0_C = ss_a0_C_s(neigh(1:n,1));
+        ab_C = ss_ab_C_s(neigh(1:n,1), neigh(1:n,1));
+        
+        l = ab_C \ a0_C;
+        S = k_covar_c0 - l'*a0_C;
+
+        LambdaM(path(i_pt), neigh_id) = l./sqrt(S);
+        LambdaM(path(i_pt), path(i_pt)) = -1/sqrt(S);
+    end
+end
+
+
+CY = sparse(LambdaM) \ transpose(inv(sparse(LambdaM)));
+
+end
--- script_server.m
+++ script_server.m
@@ -1,39 +1,129 @@
-addpath('./functions/.')
+clear all;
 
 % General setting
-Z.d=[];Z.x=[];Z.y=[];Z.n=0; % no hard data
+parm.k.covar.model = '';
+parm.k.covar.azimuth = 0;
+parm.k.covar.c0 = 1;
+parm.k.covar.alpha = 1;
+parm.seed_path = 'default';
+parm.seed_search = 'default';
+parm.seed_U = 'default';
 
-parm.k.covar(1).model = 'spherical';
-parm.k.covar(1).range0 = [15 15];
-parm.k.covar(1).azimuth = 0;
-parm.k.covar(1).c0 = .98;
-parm.k.covar(1).alpha = 1;
-parm.k.covar(2).model = 'nugget';
-parm.k.covar(2).range0 = [0 0];
-parm.k.covar(2).azimuth = 0;
-parm.k.covar(2).c0 = 0.02;
-
-parm.saveit = 0;
-parm.nscore = 0;
-parm.par = 0;
-parm.seed = 'default';
+parm.k.covar.range0 = [15 15];
+parm.k.wradius = 1.3;
+parm.mg = 0;
+nx = 100; % no multigrid
+ny = 100;
+parm.k.nb = 30;
 
 
+vario = {'exponential', 'gaussian', 'spherical', 'hyperbolic','k-bessel', 'cardinal sine'};
 
-%% Computational Saving cst path
-parm.g.scale=[7;7];
+% True
+[Y, X]=ndgrid(1:nx,1:ny);
+XY = [Y(:) X(:)];
+parfor v=1:numel(vario)
+    covar = parm.k.covar;
+    covar(1).model = vario{v};
+    covar = kriginginitiaite(covar);
+    DIST = squareform(pdist(XY*covar.cx));
+    CY_true{v} = kron(covar.g(DIST), covar.c0);
+end
+err_frob_fx = @(CY,v) sqrt(sum((CY(:)-CY_true{v}(:)).^2)) / sum((CY_true{v}(:).^2));
 
-parm.par = 0;
 
-parm.k.wradius = 2;
-parm.k.method = 'sbss';
-parm.k.nb = [0 0 0 0 0; 30 0 0 0 0];
+% Simulation
+N = 10;
+parfor v=1:numel(vario)
+    parm1=parm;
+    parm1.k.covar.model = vario{v};
+    CY=repmat({nan(ny*nx,ny*nx,2)},N,1);
+    eta{v}=cell(N,1);
+    nn=cell(N,1);
+    for n=1:(2^(N-1))
+        vec = de2bi(n-1,N);
+        CY{1}(:,:,vec(1)+1) = full(SGS_varcovar(nx,ny,parm1));
+        eta{v}{1} = [eta{v}{1}; err_frob_fx(CY{1}(:,:,vec(1)+1),v)];
+        nn{1} = [nn{1}; 2^(1-1)];
+        i=1;
+        while (vec(i)==1)
+            CY{i+1}(:,:,vec(i+1)+1) = mean(CY{i},3);
+            eta{v}{i+1} = [eta{v}{i+1}; err_frob_fx(CY{i+1}(:,:,vec(i+1)+1),v)];
+            nn{i+1} = [nn{i+1}; 2^(i)];
+            i=i+1;
+        end
+    end
+    disp(['Vario: ' num2str(v) '/' num2str(numel(vario))])
+end
 
-parm.n_realisation  = 1;
-parm.cstk = 1;
+save(['./cst_path_paper/frobenium_100n_30k_512N'],'eta','vario','nn','parm','nx','ny');
 
-[Res,t] = SGS(Z,parm);
+boxplot(cell2mat(eta{v}),cell2mat(nn),'Orientation','horizontal');
 
-t
-sum(t.krig)/t.global
-save('./cst_path_paper/T_12x12n_30k_1m_MINK','parm','t')
+
+%figure
+load('result-SGSIM/Constant Path/frobenium_v20_x512_MG');
+load('result-SGSIM/Constant Path/frobenium_v20_x512');
+
+figure(2);clf; hold on;
+lim=4;
+for v=1:numel(vario)
+    subplot(numel(vario)/2,2,v); hold on;
+    boxplot(cell2mat(eta{v}(1:lim)),cell2mat(nn(1:lim)),'Orientation','horizontal','Color',[0 113 188]/255);
+    axis tight;
+    xlabel(['Number of Path\newline' vario{v} ' variogram']);
+    ylabel(['Standardized Frobenius Norm Error'])
+end
+
+
+figure(2);clf;hold on;
+lim=8;
+for v=1:numel(vario)
+    for i=1:numel(nn)-1
+        stat_eta{v}(i,:) = [mean(eta{v}{i}) std(eta{v}{i})];
+        xx(i) = nn{i}(1);
+        [f(i,:),xi(i,:)] = ksdensity(eta{v}{i});
+        yi(i,:) = xx(i)*ones(1,numel(f(i,:)));
+        ksdensity(eta{v}{i})
+    end 
+    %F=scatteredInterpolant(xi(:),yi(:),f(:))
+    
+    [X,Y]=meshgrid(linspace(min(min(xi)),max(max(xi)),100), linspace(min(min(yi)),max(max(yi)),256));
+    
+    
+    a = F(X,Y);
+    imagesc(X(1,:),Y(:,1),ones(256,100),'alphadata',a./max(max(a)))
+   
+    
+%     plot(stat_eta{v}(:,1),xx)
+%     plot(stat_eta{v}(:,1) - stat_eta{v}(:,2),xx,'--')
+%     plot(stat_eta{v}(:,1) + stat_eta{v}(:,2),xx,'--')
+end
+ylabel('Number of Path');
+xlabel('Standardized Frobenius Norm Error')
+axis tight;
+
+figure(1); clf; hold on;
+lim=8;
+filename2={'frobenium_sph_8_512', 'frobenium_sph_20_512' ,'frobenium_sph_52_512'};
+col={'b','r','g','y'};
+for i=1:numel(filename2)
+    s=load(['result-SGSIM/Constant Path/' filename2{i}]);
+    xlim_b=get(gca,'xlim');
+    boxplot(cell2mat(s.eta(1:lim)),cell2mat(nn(1:lim)),'Orientation','horizontal','color',col{i},'symbol',['+' col{i}]);
+    xlim_a=get(gca,'xlim');
+    if i>1
+        xlim([ min(xlim_b(1), xlim_a(1)) max(xlim_b(2), xlim_a(2))])
+    end
+end
+hLegend = legend(findall(gca,'Tag','Box'), {'Neighboors: 8', 'Neighboors: 20', 'Neighboors: 52'});
+ylabel('Number of Path');
+xlabel('Standardized Frobenius Norm Error')
+
+
+lim=8;
+for v=1:numel(vario)
+   (mean(eta{v}{5})-mean(eta{v}{1}))./mean(eta{v}{1})
+end
+
+
